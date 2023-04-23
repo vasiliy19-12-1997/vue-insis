74a10c58cba01c7a7ce2533cd1f301f94044d02a
vasiliy19-12-1997 <Wasyok.ru@yandex.ru>
2023-04-23 11:22:51 +0500

    start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Catalog from '@/components/Catalog.vue'
 </script>
-
 <template>
   <main>
-    <TheWelcome />
+    <Catalog />
   </main>
 </template>
